06275440800bcf3603e503aa34623da0ebae762a
Doron Segal <doron2402@gmail.com>
2015-08-23 11:05:20 -0700

    tests

--- test/onPreResponse.js
+++ test/onPreResponse.js
@@ -6,81 +6,83 @@ var rewire = require('rewire');
 var plugin = rewire('../src/plugin.js');
 
 describe('onPreResponse', function() {
+  var incrementName = [];
+  var timingName = [];
+  var statsdCall = {
+    increment: [
+        'response.out.Total.counter',
+        'response.out.test.endpoint.counter',
+        'response.out.test.endpoint.200.counter',
+        'response.out.Total.200.counter',
+        'request.in.test.endpoint.counter',
+        'request.in.Total.counter'
+    ],
+    timing: [
+        { name: 'request.Total.timer' },
+        { name: 'request.test.endpoint.timer' }
+      ]
+    };
   describe('Plugin', function() {
-      var incrementName = [];
-      var timingName = [];
-      var statsdCall = {
-          increment: [
-            'request.in.test.endpoint.counter',
-            'request.in.Total.counter',
-            'response.out.Total.counter',
-            'response.out.Total.200.counter',
-            'request.out.test.endpoint.counter',
-            'request.out.test.endpoint.200.counter'
-        ],
-          timing: [
-                { name: 'request.Total.timer' },
-                { name: 'request.test.endpoint.timer' }
-            ]
-      };
     before(function(done){
-        var settings = {
-            host: 'statsd.localhost',
-            prefix: 'search-api-node.development.local.',
-            port: 8125,
-            debug: true
-        };
-        plugin.register({
-            ext: function(_, handler) {
-                plugin.__set__('sdc', {
-                    increment: function(name) {
-                        incrementName.push(name);
-                    },
-                    timing: function(name, value) {
-                        timingName.push({name : name, value: value});
-                    }
-                });
+      var settings = {
+        host: 'statsd.localhost',
+        prefix: 'search-api-node.development.local.',
+        port: 8125,
+        debug: true
+      };
+      plugin.register({
+        ext: function(_, handler) {
+          plugin.__set__('sdc', {
+            increment: function(name) {
+              incrementName.push(name);
+            },
+            timing: function(name, value) {
+              timingName.push({name : name, value: value});
+            }
+          });
 
-                handler({
-                    info: {
-                        received: new Date()
-                    },
-                    method: 'get',
-                    response: {
-                        statusCode: 200
-                    },
-                    url: { pathname: '/test/endpoint' },
-                    route: {
-                        settings: {
-                            plugins: {
-                                'hapi-statsd': {
-                                    endpoint: 'test/endpoint',
-                                    version: 'test-version'
-                                }
-                            }
-                        }
-                    }
-                }, {
-                    continue: function() {
-                        done();
-                    }
-                });
+          handler({
+            info: {
+              received: new Date()
+            },
+            method: 'get',
+            response: {
+              statusCode: 200
+            },
+            url: { pathname: '/test/endpoint' },
+            route: {
+              settings: {
+                plugins: {
+                  'hapi-statsd': {
+                    endpoint: 'test/endpoint',
+                    version: 'test-version'
+                  }
                 }
-            }, settings, function() {});
-        });
+              }
+            }
+          }, {
+            continue: function() {
+              done();
+            }
+          });
+        }
+      }, settings, function() {});
+    });
 
-        incrementName.forEach(function (value, i){
-            it('Counter should be equal : `' + statsdCall.increment[i] + '`', function() {
-                expect(value).to.equal(statsdCall.increment[i]);
-            });
+    describe('Increment', function(){
+        statsdCall.increment.forEach(function (value, i){
+          it('Counter should be equal : `' + statsdCall.increment[i] + '`', function() {
+            expect(value).to.equal(statsdCall.increment[i]);
+          });
         });
-
-        statsdCall.timing.forEach(function(value, i){
-            it('Timer Should be equal : `' + statsdCall.timing[i].name + '`', function(){
-                expect(timingName[i].name).to.equal(statsdCall.timing[i].name);
-                expect(timingName[i].value).to.exist;
-           });
+    });
+    describe('Timing', function(){
+      statsdCall.timing.forEach(function(value, i){
+        it('Timer Should be equal : `' + statsdCall.timing[i].name + '`', function(){
+          expect(timingName[i].name).to.equal(statsdCall.timing[i].name);
+          expect(timingName[i].value).to.exist;
         });
-
+      });
     });
   });
+});
